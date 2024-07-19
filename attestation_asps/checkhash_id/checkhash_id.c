43ecbce2cef60f9eba15a73fdc74356341aefb56
Adam Petz <ampetz@ku.edu>
2024-07-19 00:36:42 -0500

    adding some appraisal ASPs, twekaing ASP outputs (dummy strings)

--- attestation_asps/checkhash_id/checkhash_id.c
+++ attestation_asps/checkhash_id/checkhash_id.c
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
   ASPRunRequest req = ASPRunRequest_from_string(argv[1]);
-  const char *preamble = "hashFile(\0";
+  const char *preamble = "checkHash(\0";
   const char *postamble = ")\0";
   size_t resp_ev_size = strlen(preamble) + (req.raw_ev == NULL ? 0 : strlen(req.raw_ev->ev_val)) + strlen(postamble);
   char *resp_ev = (char *)malloc(sizeof(char) * resp_ev_size);
@@ -16,6 +16,5 @@ int main(int argc, char **argv)
   strcat(resp_ev, postamble);
   ASPRunResponse resp = {true, build_RawEv_T(resp_ev)};
   printf("%s", ASPRunResponse_to_string(resp));
-  // printf("{ \"TYPE\": \"RESPONSE\", \"ACTION\": \"ASP_RUN\", \"SUCCESS\": true, \"PAYLOAD\": [\"attesting\"]}");
   return 0;
 }
