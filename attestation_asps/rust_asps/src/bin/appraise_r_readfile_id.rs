6ae4565ab83b0119f73f99692de80b8b67b484eb
Adam Petz <ampetz@ku.edu>
2024-10-16 16:03:59 +1100

    experiments with appraisal asps (source files need cleaning up still...)

--- attestation_asps/rust_asps/src/bin/appraise_r_readfile_id.rs
+++ attestation_asps/rust_asps/src/bin/appraise_r_readfile_id.rs
@@ -7,13 +7,63 @@ use anyhow::{Context, Result};
 use std::env;
 use hex;
 
+use std::collections::HashMap;
+
 // Packages required to perform specific ASP action.
 // e.g.
 use sha2::{Sha256, Digest};
 
+/*
+
+fn main() {
+    let mut mymap = HashMap::new();
+    mymap.insert("foo".to_string(), "bar".to_string());
+    println!("{0}", mymap["foo"]);
+    println!("{0}", mymap.get("foo").unwrap());
+}
+
+*/
+
+/*
+Definition cds_config_1_targ : TARG_ID := "cds_config_1_targ".
+Definition cds_config_2_targ : TARG_ID := "cds_config_2_targ".
+*/
+
+
 // function where the work of the ASP is performed.
 // May signal an error which will be handled in main.
 fn body() -> Result<String> {
+    
+
+    let golden_filename_key = "cds_config_1_targ";
+    let golden_filename_key_2 = "cds_config_2_targ";
+    let golden_filename_key_3 = "cds_config_3_targ";
+
+
+    let golden_filename = 
+        "/Users/adampetz/Documents/Summer_2024/asp-libs/attestation_asps/rust_asps/golden_files/targFileGolden.txt";
+    
+    let golden_filename_2 = 
+        "/Users/adampetz/Documents/Summer_2024/asp-libs/attestation_asps/rust_asps/golden_files/targFileGolden_2.txt";
+
+    let golden_filename_3 = 
+        "/Users/adampetz/Documents/Summer_2024/asp-libs/attestation_asps/rust_asps/golden_files/targFileGolden_3.txt";
+    let mut goldenPathMap = HashMap::new();
+    goldenPathMap.insert(golden_filename_key.to_string(), golden_filename.to_string());
+    goldenPathMap.insert(golden_filename_key_2.to_string(), golden_filename_2.to_string());
+    goldenPathMap.insert(golden_filename_key_3.to_string(), golden_filename_3.to_string());
+
+
+    /*
+    let goldenPath_targFile = goldenPathMap[golden_filename_key].clone();
+    let goldenPath_targFile_2 = goldenPathMap[golden_filename_key_2].clone();
+
+    println!("{0}", goldenPath_targFile);
+    println!("{0}", goldenPath_targFile_2);
+
+    */
+
+
 
     // For every ASP, an ASPRunRequest appears as the single command-line argument
     let args: Vec <String> = env::args().collect();
@@ -32,7 +82,20 @@ fn body() -> Result<String> {
     //let args_map = req.ASP_ARGS;
     //let filename = &args_map.get("filepath").context("filename argument not provided to ASP, readfile_id")?;
 
-    let golden_filename = "/Users/adampetz/Documents/Summer_2024/asp-libs/attestation_asps/rust_asps/golden_files/targFileGolden.txt";
+    let golden_filename_key_targid = req.ASP_TARG_ID;
+    let golden_filename = goldenPathMap[&golden_filename_key_targid].clone();
+    //let goldenPath_targFile_2 = goldenPathMap[golden_filename_key_2].clone();
+
+    //println!("\n\n\n\n\n\n\n\n{0}\n\n\n\n\n\\n", golden_filename);
+    //println!("{0}", goldenPath_targFile_2);
+
+
+    //println!("\n\n\n\n\n\n\n\nHIHIHIHIHIHIHIHIHIHIHIHIHI\n\n\n\n\n\n\n\n\n\n\n");
+
+
+
+
+    //let golden_filename = "/Users/adampetz/Documents/Summer_2024/asp-libs/attestation_asps/rust_asps/golden_files/targFileGolden.txt";
     let bytes = std::fs::read(golden_filename)?; // Vec<u8>
 
     //Code sample for accessing input evidence within the ASP.
@@ -44,7 +107,7 @@ fn body() -> Result<String> {
     let latest_evidence = &evidence_in[0];
     //print!("\n\n\n\nLatest Evidence: {}\n\n\n\n", latest_evidence);
 
-    let golden_bytes_string = hex::encode_upper(&bytes);
+    //let golden_bytes_string = hex::encode_upper(&bytes);
     //print!("\n\n\n\nGolden Bytes String: {}\n\n\n\n", golden_bytes_string);
 
 
@@ -77,6 +140,8 @@ fn body() -> Result<String> {
             false => {"FAILED".to_string()}
         };
 
+    //print!("\n\n\n\nout_contents String: {}\n\n\n\n", out_contents);
+
     let out_contents_hex: String = hex::encode_upper(out_contents);
 
     //let out_contents_b64 = base64::encode(out_contents_hex);
@@ -87,7 +152,8 @@ fn body() -> Result<String> {
 
     // Step 3:
     // Construct the ASPRunResponse with this evidence.
-    let  response = successfulASPRunResponse (evidence);
+
+    let response = successfulASPRunResponse (evidence);
     let response_json = serde_json::to_string(&response)?;
     Ok (response_json)
 }
