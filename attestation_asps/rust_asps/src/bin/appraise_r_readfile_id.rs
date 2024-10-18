d02438aa32653456bb2553d6d59e0c3d4cadb9f0
Adam Petz <ampetz@ku.edu>
2024-10-18 11:14:19 +1100

    cleaning up comments

--- attestation_asps/rust_asps/src/bin/appraise_r_readfile_id.rs
+++ attestation_asps/rust_asps/src/bin/appraise_r_readfile_id.rs
@@ -13,21 +13,6 @@ use std::collections::HashMap;
 // e.g.
 use sha2::{Sha256, Digest};
 
-/*
-
-fn main() {
-    let mut mymap = HashMap::new();
-    mymap.insert("foo".to_string(), "bar".to_string());
-    println!("{0}", mymap["foo"]);
-    println!("{0}", mymap.get("foo").unwrap());
-}
-
-*/
-
-/*
-Definition cds_config_1_targ : TARG_ID := "cds_config_1_targ".
-Definition cds_config_2_targ : TARG_ID := "cds_config_2_targ".
-*/
 
 
 // function where the work of the ASP is performed.
@@ -54,16 +39,6 @@ fn body() -> Result<String> {
     goldenPathMap.insert(golden_filename_key_3.to_string(), golden_filename_3.to_string());
 
 
-    /*
-    let goldenPath_targFile = goldenPathMap[golden_filename_key].clone();
-    let goldenPath_targFile_2 = goldenPathMap[golden_filename_key_2].clone();
-
-    println!("{0}", goldenPath_targFile);
-    println!("{0}", goldenPath_targFile_2);
-
-    */
-
-
 
     // For every ASP, an ASPRunRequest appears as the single command-line argument
     let args: Vec <String> = env::args().collect();
@@ -79,23 +54,12 @@ fn body() -> Result<String> {
     // Code for specific for this ASP.
     // This example computes the HASH of the file named in an argument for the ASP.
     // May return an Err Result, which will be captured in main.
-    //let args_map = req.ASP_ARGS;
-    //let filename = &args_map.get("filepath").context("filename argument not provided to ASP, readfile_id")?;
+
 
     let golden_filename_key_targid = req.ASP_TARG_ID;
     let golden_filename = goldenPathMap[&golden_filename_key_targid].clone();
-    //let goldenPath_targFile_2 = goldenPathMap[golden_filename_key_2].clone();
-
-    //println!("\n\n\n\n\n\n\n\n{0}\n\n\n\n\n\\n", golden_filename);
-    //println!("{0}", goldenPath_targFile_2);
 
 
-    //println!("\n\n\n\n\n\n\n\nHIHIHIHIHIHIHIHIHIHIHIHIHI\n\n\n\n\n\n\n\n\n\n\n");
-
-
-
-
-    //let golden_filename = "/Users/adampetz/Documents/Summer_2024/asp-libs/attestation_asps/rust_asps/golden_files/targFileGolden.txt";
     let bytes = std::fs::read(golden_filename)?; // Vec<u8>
 
     //Code sample for accessing input evidence within the ASP.
@@ -105,24 +69,11 @@ fn body() -> Result<String> {
     let evidence_in = match req.RAWEV {RawEv::RawEv(x) => x,};
 
     let latest_evidence = &evidence_in[0];
-    //print!("\n\n\n\nLatest Evidence: {}\n\n\n\n", latest_evidence);
 
-    //let golden_bytes_string = hex::encode_upper(&bytes);
-    //print!("\n\n\n\nGolden Bytes String: {}\n\n\n\n", golden_bytes_string);
-
-
-
-    // Evidence is always base64 encoded, so decode this
+    // Evidence is always hex encoded, so decode this
     let file_bytes = hex::decode(latest_evidence)?;
     let bytes_equal : bool = bytes.eq(&file_bytes);
 
-    //let filename = String::from_utf8(filename_bytes)?;
-
-
-
-
-
-    //let hash = Sha256::digest(&bytes);
 
     // End of code specific for this ASP.
 
@@ -130,9 +81,6 @@ fn body() -> Result<String> {
     // Step 1:
     // The return value for an ASP, must be
     // encoded in BASE64, and converted to ascii for JSON transmission
-    //let hash_b64: String = base64::encode(hash);
-
-    //let contents_b64: String = base64::encode(bytes);
 
     let out_contents: String =
         match bytes_equal {
@@ -140,11 +88,7 @@ fn body() -> Result<String> {
             false => {"FAILED".to_string()}
         };
 
-    //print!("\n\n\n\nout_contents String: {}\n\n\n\n", out_contents);
-
     let out_contents_hex: String = hex::encode_upper(out_contents);
-
-    //let out_contents_b64 = base64::encode(out_contents_hex);
 
     // Step 2:
     // wrap the value as Evidence
